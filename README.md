adfe60d5366784f57740f3c1650f83ff4d921ef5
LLSolucaoDesigner <llsolucaodesigner@gmail.com>
2022-10-10 15:29:06 -0300

    Iniciando Projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+# Projeto  Treinamento - Clone OLX
+
+## Desenvolvimento em React.js
+
+### Node versão 16.17.0
+
+Projeto desenvolvido curso *FullStack JS*
+Da empresa _SoiTech_
+
+Author: *Luan Farias*
---
+++ src/pages/NotFound/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const NotFoundArea = styled.div`
+background-color: #BA55D3;
+
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+    align-item: center;
+    justify-content: center;
+    display: flex;
+
+    h2 {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+}
+`
